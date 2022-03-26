301258eac98737594e1803e86e90c438ea3957f7
emsthf <deunsol916@gmail.com>
2022-03-26 11:48:14 +0900

    add Doing table

---
+++ src/main/java/com/jo/goal/service/DoingService.java
@@ -0,0 +1,14 @@
+package com.jo.goal.service;
+
+import com.jo.goal.model.Doing;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DoingService {
+    Doing addDoing(Doing doing);
+    Doing editDoing(Doing doing);
+    List<Doing> getAllDoing();
+    Optional<Doing> getDoingById(Long id);
+    void delDoing(Long id);
+}
